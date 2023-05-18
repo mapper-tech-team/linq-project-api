035e96c80ff8ae588eaffd3fbeb932e118dc57a2
mapper-tech-team <mappertech@gmail.com>
2023-05-18 19:41:36 -0300

    Implementação dos testes de unidade

---
+++ src/test/java/com/example/linqprojectapi/controller/AlunoControllerTest.java
@@ -0,0 +1,8 @@
+package com.example.linqprojectapi.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AlunoControllerTest {
+}
